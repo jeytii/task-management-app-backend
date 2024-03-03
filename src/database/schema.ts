d71aaeac88f8b57f9c2fd20e85fa900e70cbd230
Jonathan Tarnate <jstarnate16@gmail.com>
2024-03-03 22:19:37 +0800

    Add relationship constraints

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,4 +1,5 @@
-import { boolean, date, pgTable, text, timestamp, uniqueIndex, uuid, varchar } from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
+import { type AnyPgColumn, boolean, date, pgTable, text, timestamp, uniqueIndex, uuid, varchar } from 'drizzle-orm/pg-core'
 
 export const users = pgTable(
   'users',
@@ -7,8 +8,8 @@ export const users = pgTable(
     name: varchar('name').notNull(),
     username: varchar('username').unique().notNull(),
     password: varchar('password').notNull(),
-    createdAt: timestamp('createdAt', { withTimezone: true }).defaultNow().notNull(),
-    updatedAt: timestamp('updatedAt', { withTimezone: true }).defaultNow().notNull()
+    createdAt: timestamp('createdAt', { mode: 'string' }).defaultNow().notNull(),
+    updatedAt: timestamp('updatedAt', { mode: 'string' }).defaultNow().notNull()
   },
   (table) => ({
     usernameIdx: uniqueIndex('usernameIdx').on(table.username)
@@ -19,14 +20,16 @@ export const tasks = pgTable(
   'tasks',
   {
     id: uuid('id').defaultRandom().primaryKey(),
-    userId: varchar('userId').references(() => users.id, { onDelete: 'cascade' }).notNull(),
-    parentId: varchar('parentId'),
-    priorityId: varchar('priorityId').references(() => priorities.id),
+    userId: uuid('userId').references(() => users.id, { onDelete: 'cascade' }).notNull(),
+    parentId: uuid('parentId').references((): AnyPgColumn => tasks.id),
+    priorityId: uuid('priorityId').references(() => priorities.id),
     title: varchar('title').notNull(),
     description: text('description').notNull(),
     startDate: date('startDate', { mode: 'string' }),
     dueDate: date('dueDate', { mode: 'string' }),
-    finished: boolean('finished').default(false)
+    finished: boolean('finished').default(false),
+    createdAt: timestamp('createdAt', { mode: 'string' }).defaultNow().notNull(),
+    updatedAt: timestamp('updatedAt', { mode: 'string' }).defaultNow().notNull()
   }
 )
 
@@ -35,7 +38,9 @@ export const priorities = pgTable(
   {
     id: uuid('id').defaultRandom().primaryKey(),
     name: varchar('name').notNull(),
-    color: varchar('color').notNull()
+    color: varchar('color').notNull(),
+    createdAt: timestamp('createdAt', { mode: 'string' }).defaultNow().notNull(),
+    updatedAt: timestamp('updatedAt', { mode: 'string' }).defaultNow().notNull()
   }
 )
 
@@ -43,8 +48,10 @@ export const tags = pgTable(
   'tags',
   {
     id: uuid('id').defaultRandom().primaryKey(),
-    userId: varchar('userId').references(() => users.id, { onDelete: 'cascade' }).notNull(),
-    name: varchar('name').notNull()
+    userId: uuid('userId').references(() => users.id, { onDelete: 'cascade' }).notNull(),
+    name: varchar('name').notNull(),
+    createdAt: timestamp('createdAt', { mode: 'string' }).defaultNow().notNull(),
+    updatedAt: timestamp('updatedAt', { mode: 'string' }).defaultNow().notNull()
   }
 )
 
@@ -52,8 +59,35 @@ export const settings = pgTable(
   'settings',
   {
     id: uuid('id').defaultRandom().primaryKey(),
-    userId: varchar('userId').references(() => users.id, { onDelete: 'cascade' }).notNull(),
+    userId: uuid('userId').references(() => users.id, { onDelete: 'cascade' }).notNull(),
     hideFinishedTasks: boolean('hideFinishedTasks').default(true),
-    darkMode: boolean('darkMode').default(false)
+    darkMode: boolean('darkMode').default(false),
+    createdAt: timestamp('createdAt', { mode: 'string' }).defaultNow().notNull(),
+    updatedAt: timestamp('updatedAt', { mode: 'string' }).defaultNow().notNull()
   }
 )
+
+export const usersRelations = relations(users, ({ one, many }) => ({
+  tasks: many(tasks),
+  tags: many(tags),
+  settings: one(settings)
+}))
+
+export const tasksRelations = relations(tasks, ({ one, many }) => ({
+  user: one(users),
+  priority: one(priorities),
+  subtasks: many(tasks),
+  parent: one(tasks)
+}))
+
+export const prioritiesRelations = relations(priorities, ({ many }) => ({
+  tasks: many(tasks)
+}))
+
+export const tagsRelations = relations(priorities, ({ one }) => ({
+  user: one(users)
+}))
+
+export const settingsRelations = relations(settings, ({ one }) => ({
+  user: one(users)
+}))
